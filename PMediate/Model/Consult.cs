340ef43f5212991a2aa048e8ffab6112807d3871
Usamn Ali <usmanaliua.uali@gmail.com>
2020-12-27 01:25:34 -0800

    Model

---
+++ PMediate/Model/Consult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PMediate.Model
+{
+    public class Consult
+    {
+        public Consult(long clientId, DateTime startDate, DateTime endDate)
+        {
+            ClientId = clientId;
+            DateRange = DateRange.Create(startDate, endDate).Value;
+        }
+
+        private Consult()
+        {
+
+        }
+
+        public long Id { get; private set; }
+        public long ClientId { get; private set; }
+        public DateRange DateRange { get; private set; }
+
+        public void Reschadule(DateTime startDate, DateTime endDate)
+        {
+            DateRange = DateRange.Create(startDate, endDate).Value;
+        }
+    }
+}
